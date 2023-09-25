40d378e7e0774091c2df46229f096cc6a5709ba6
ChewingOne <2194315388@qq.com>
2023-09-25 23:20:52 +0800

    update
    666

--- suduko.py
+++ suduko.py
@@ -1,10 +1,9 @@
+import http.server  # 导入http.server模块
 import json
 import random
-from multiprocessing import Pool
 import webbrowser
-import os
-import http.server  # 导入http.server模块
-import urllib.parse  # 导入urllib.parse模块
+from multiprocessing import Pool
+
 
 # 自定义处理程序类，继承自http.server.SimpleHTTPRequestHandler
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
@@ -12,6 +11,7 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
         # 添加 "x-content-type-options" 头部到响应中
         self.send_header('x-content-type-options', 'nosniff')
         super().end_headers()
+
 
 def generate_sudoku():
     # 生成一个空的数独游戏板
@@ -26,6 +26,7 @@ def generate_sudoku():
     solve_sudoku(board)
 
     return board
+
 
 def is_valid(board, row, col, num):
     # 检查行是否合法
@@ -45,6 +46,7 @@ def is_valid(board, row, col, num):
 
     return True
 
+
 def solve_sudoku(board):
     for row in range(9):
         for col in range(9):
@@ -59,8 +61,7 @@ def solve_sudoku(board):
     return True
 
 
-def create_sudoku_data(index,difficulty):
-    
+def create_sudoku_data(index, difficulty):
     sudoku_board = generate_sudoku()
 
     # 保存完整的数独数据到 JSON 文件（例如，sudoku_data_full_30.json）
@@ -84,6 +85,7 @@ def create_sudoku_data(index,difficulty):
 
     return {"puzzle": sudoku_board, "removed_positions": removed_positions}
 
+
 def write_sudoku_data_to_json(difficulty):
     all_sudoku_data = []
 
@@ -96,7 +98,7 @@ def write_sudoku_data_to_json(difficulty):
         filename = f"sudoku_data_{difficulty}_{i + 1}.json"
         with open(filename, "w") as f:
             json.dump(data, f)
-    
+
     # 将每个数独游戏数据添加到列表中
     for i, data in enumerate(sudoku_data):
         all_sudoku_data.append(data)
@@ -104,11 +106,12 @@ def write_sudoku_data_to_json(difficulty):
     filename = f"all_sudoku_data_{difficulty}.json"
     # 将整个列表写入一个 JSON 文件
     with open(filename, "w") as f:
-        json.dump(all_sudoku_data, f)   
+        json.dump(all_sudoku_data, f)
 
-
-#def open_index_html():
+    # def open_index_html():
     # 打开同一目录下的index.html文件
+
+
 #    webbrowser.open("file://" + os.path.abspath("index.html"))
 
 if __name__ == "__main__":
@@ -116,14 +119,14 @@ if __name__ == "__main__":
     write_sudoku_data_to_json(30)  # 生成简单难度的数据
     write_sudoku_data_to_json(40)  # 生成中等难度的数据
     write_sudoku_data_to_json(50)  # 生成困难难度的数据
-        
+
     # 启动本地服务器，监听端口
     server_address = ('', 8000)  # 可以根据需要选择任何可用的端口
     httpd = http.server.HTTPServer(server_address, MyRequestHandler)
-    
+
     # 打开默认浏览器并访问服务器地址
     webbrowser.open("http://localhost:8000/index.html")
-    
+
     try:
         # 启动服务器，Ctrl+C 停止服务器
         print("本地服务器已启动，按 Ctrl+C 停止服务器")

d2f3845b5101056b38c1eb9c2cc215832d490dae
Cheng-lian <97796169+Cheng-lian@users.noreply.github.com>
2023-10-06 23:12:56 +0800

    searchanswer

--- all_sudoku_data_50.js
+++ all_sudoku_data_50.js
@@ -52,3 +52,58 @@ function renderSudoku(sudokuData, container) {
   // 将表格添加到数独容器中
   container.appendChild(table);
 }
+
+// 获取查看答案按钮
+const showAllAnswersButton = document.getElementById('searchanswerButton');
+
+// 为按钮添加点击事件监听器
+showAllAnswersButton.addEventListener('click', function () {
+    // 使用循环加载并渲染所有答案数据
+    for (let i = 1; i <= 9; i++) {
+        // 构建答案JSON文件路径
+        const answerFilePath = `sudoku_data_solved_50_${i}.json`;
+        
+        // 获取对应数独容器的ID
+        const containerId = `sudoku-container-${i}`;
+        const sudokuContainer = document.getElementById(containerId);
+
+        // 加载答案数据的JSON文件
+        fetch(answerFilePath)
+            .then(response => response.json())
+            .then(answerData => {
+                
+                console.log(answerData)
+                // 清空数独容器
+                
+                sudokuContainer.innerHTML = '';
+
+                // 渲染答案到数独容器中
+                renderSudoku(answerData.solution, sudokuContainer);
+            })
+            .catch(error => {
+                console.error('Error loading answer data:', error);
+            });
+    }
+});
+
+// 渲染数独游戏的函数
+function renderSudoku(sudokuData, container) {
+    // 创建表格元素
+    const table = document.createElement("table");
+    table.className = "sudoku-table"; // 添加一个CSS类名以便样式化
+
+    // 遍历数独游戏数据并渲染每个格子
+    for (let i = 0; i < 9; i++) {
+        const row = document.createElement("tr");
+        for (let j = 0; j < 9; j++) {
+            const cell = document.createElement("td");
+            cell.textContent = sudokuData[i][j] !== 0 ? sudokuData[i][j] : "";
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+
+    // 将表格添加到数独容器中
+    container.appendChild(table);
+}
+
